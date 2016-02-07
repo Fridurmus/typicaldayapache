726000a310a8784179dcfab46896f16f78815758
Sean Davis <fridurmus@gmail.com>
2016-02-07 16:16:14 -0500

    Google Map Initial Config

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,3 +1,14 @@
+function initMap(){
+		var mapDiv = document.getElementById("map");
+		var map = new google.maps.Map(mapDiv, {
+			center: {lat: 28.121095, lng: -80.6296357},
+			zoom: 17,
+			draggable: false,
+			mapTypeId: "hybrid"
+		});
+	}
+
 $(document).ready(function(){
 	$("h1").append(" Jquery is working.");
+	
 })
