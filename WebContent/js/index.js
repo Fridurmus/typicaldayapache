b68e917fe09263826bf9016fc0317e5be6ecf0f2
Sean Davis <fridurmus@gmail.com>
2016-02-08 03:20:46 -0500

    Removed Sass, updated buttons

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -3,6 +3,10 @@ var map;
 var homeLatLng = {lat: 28.121095, lng: -80.627447}; //home
 var phiLatLng = {lat: 28.1688249, lng: -80.667917}; //philosophy class
 var freLatLng = {lat: 28.1693049, lng: -80.6707692}; //french class
+var mornTime = "Home: 00:00 - 14:45";
+var phiTime = "Philosophy: 15:15 - 16:30";
+var freTime = "French I: 18:00 - 20:45";
+var niteTime = "Home: 21:00 - 23:59";
 
 //Initialize the map pointing at home
 function initMap(){
@@ -32,11 +36,12 @@ function initMap(){
 	}
 
 //set up handlers for each button
-function pan(loc){
+function pan(loc, time){
 	map.panTo(loc);
+	$("h1").text(time)
 }
 //JQuery on document load test
 $(document).ready(function(){
-	$("h1").append(" Jquery is working.");
+	$("h1").append(" Home: 00:00 - 14:45");
 	
 })
